8dd879f3e83c4fbe2a0672a4cad7b07070e1d8b9
Steven Lee <stevenl1688@gmail.com>
2024-12-27 00:57:34 -0800

    new update from chatgpt

---
+++ script.js
@@ -0,0 +1,8 @@
+// You can add interactive features in JavaScript if necessary
+document.getElementById('download-resume').addEventListener('click', function() {
+    alert('Your resume is now downloading!');
+});
+
+document.getElementById('view-certificates').addEventListener('click', function() {
+    alert('You are viewing the certificate!');
+});
